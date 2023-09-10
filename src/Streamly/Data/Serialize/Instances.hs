ce7aab471d73a01df33a677eca4bbad0b873d4ce
Adithya Kumar <adithya@composewell.com>
2023-09-10 11:56:09 +0530

    Implement the basic serialization instances

--- src/Streamly/Data/Serialize/Instances.hs
+++ src/Streamly/Data/Serialize/Instances.hs
@@ -2,10 +2,10 @@
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE RecordWildCards #-}
 
--- XXX Fix the derivation in streamly and remove this.
-{-# OPTIONS_GHC -Wno-unused-matches #-}
+-- This is required as all the instances in this module are orphan instances.
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Streamly.Data.Serialize.Instances where
+module Streamly.Data.Serialize.Instances () where
 
 --------------------------------------------------------------------------------
 -- Imports
@@ -13,112 +13,61 @@ module Streamly.Data.Serialize.Instances where
 
 import Streamly.Internal.Data.Serialize (Serialize(..))
 import Data.Text.Internal (Text(..))
-import Streamly.Internal.Data.Array (Array(..))
 import Streamly.Internal.Data.Unbox (MutableByteArray(..))
 import Data.Maybe (fromJust)
+import Data.Fixed (Fixed)
 
 import qualified Data.Text.Array as TArr (Array(..))
 import qualified Streamly.Internal.Data.Unbox as Unbox
 import qualified Streamly.Internal.Data.Serialize.TH as Serialize
-import qualified Streamly.Internal.Data.MutArray as MutArray
-import qualified Streamly.Internal.Data.Array as Array
 
--- XXX Only import the required
-import Data.Time
--- import Data.Time.Calendar.OrdinalDate
--- import Data.Time.LocalTime
--- import Language.Haskell.TH
-import Prelude
+import Data.Time (Day, TimeOfDay, LocalTime, DiffTime, UTCTime)
+
 import GHC.Exts
-import GHC.Integer.GMP.Internals
-import Data.Fixed
+
+import Data.Scientific (Scientific)
 
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Vector as Vector
-import qualified Data.Scientific as Scientific
 
 --------------------------------------------------------------------------------
--- Common Instances
+-- Time
 --------------------------------------------------------------------------------
 
-instance Serialize (Array a) where
-    {-# INLINE size #-}
-    size i (Array {..}) = i + (arrEnd - arrStart) + 8
-
-    {-# INLINE deserialize #-}
-    deserialize off arr end = do
-        (off1, byteLen) <- deserialize off arr end :: IO (Int, Int)
-        let off2 = off1 + byteLen
-        let slice = MutArray.MutArray arr off1 off2 off2
-        newArr <- MutArray.clone slice
-        pure (off2, Array.unsafeFreeze newArr)
-
-    {-# INLINE serialize #-}
-    serialize off arr (Array {..}) = do
-        let arrLen = arrEnd - arrStart
-        off1 <- serialize off arr arrLen
-        Unbox.putSliceUnsafe arrContents arrStart arr off1 arrLen
-        pure (off1 + arrLen)
-
-unpackInt :: Int -> Int#
-unpackInt (I# i#) = i#
-
-data LiftedInteger
-    = LIS Int
-    | LIP (Array Word)
-    | LIN (Array Word)
-
-$(Serialize.deriveSerialize ''LiftedInteger)
-
-liftInteger :: Integer -> LiftedInteger
-liftInteger (S# x) = LIS (I# x)
-liftInteger (Jp# (BN# x)) =
-    LIP (Array (MutableByteArray (unsafeCoerce# x)) 0 (I# (sizeofByteArray# x)))
-liftInteger (Jn# (BN# x)) =
-    LIN (Array (MutableByteArray (unsafeCoerce# x)) 0 (I# (sizeofByteArray# x)))
-
-unliftInteger :: LiftedInteger -> Integer
-unliftInteger (LIS (I# x)) = S# x
-unliftInteger (LIP (Array (MutableByteArray x) _ _)) =
-    Jp# (BN# (unsafeCoerce# x))
-unliftInteger (LIN (Array (MutableByteArray x) _ _)) =
-    Jn# (BN# (unsafeCoerce# x))
-
-$(Serialize.deriveSerialize ''E12)
-$(Serialize.deriveSerialize ''Fixed)
-
-instance Serialize Integer where
-    size i a = size i (liftInteger a)
-
-    {-# INLINE deserialize #-}
-    deserialize off arr end = fmap unliftInteger <$> deserialize off arr end
-
-    {-# INLINE serialize #-}
-    serialize off arr val = serialize off arr (liftInteger val)
-
-$(Serialize.deriveSerialize ''Maybe)
+$(Serialize.deriveSerializeWith
+      Serialize.defaultConfig
+      [d|instance Serialize (Fixed a)|])
 $(Serialize.deriveSerialize ''Day)
 $(Serialize.deriveSerialize ''TimeOfDay)
 $(Serialize.deriveSerialize ''LocalTime)
 $(Serialize.deriveSerialize ''DiffTime)
 $(Serialize.deriveSerialize ''UTCTime)
 
+--------------------------------------------------------------------------------
+-- Scientific
+--------------------------------------------------------------------------------
+
+$(Serialize.deriveSerialize ''Scientific)
+
+--------------------------------------------------------------------------------
+-- Text
+--------------------------------------------------------------------------------
+
 instance Serialize Text where
     size i (Text (TArr.Array _) _ len16) =
             i + len16 * 2 + 8 -- XXX sizeof Int
 
-    -- XXX Need to check the bounds here
     {-# INLINE deserialize #-}
     deserialize off arr end = do
         (off1, len16) <- deserialize off arr end :: IO (Int, Int)
         let lenBytes = len16 * 2
 
-        -- XXX Check the available length in input buffer
+        -- Check the available length in input buffer
         if (off1 + lenBytes <= end)
         then do
             newArr <- Unbox.newBytes lenBytes
-            -- XXX We can perform an unrolled word copy directly
+            -- XXX We can perform an unrolled word copy directly?
             Unbox.putSliceUnsafe arr off1 newArr 0 lenBytes
             pure
                 ( off1 + lenBytes
@@ -142,6 +91,12 @@ instance Serialize Text where
             lenBytes
         pure (off1 + lenBytes)
 
+--------------------------------------------------------------------------------
+-- BSL.ByteString
+--------------------------------------------------------------------------------
+
+-- TODO: Serialize it independently
+
 -- XXX Extremely inefficient serialization of BSL.ByteString
 -- XXX Depends on list serialization
 instance Serialize BSL.ByteString where
@@ -155,6 +110,12 @@ instance Serialize BSL.ByteString where
     {-# INLINE serialize #-}
     serialize off arr val = serialize off arr (BSL.unpack val)
 
+--------------------------------------------------------------------------------
+-- Aeson.Value
+--------------------------------------------------------------------------------
+
+-- TODO: Serialize it independently
+
 -- XXX Extremely inefficient serialization of Aeson.Value
 instance Serialize Aeson.Value where
     {-# INLINE size #-}
@@ -167,6 +128,12 @@ instance Serialize Aeson.Value where
     {-# INLINE serialize #-}
     serialize off arr val = serialize off arr (Aeson.encode val)
 
+--------------------------------------------------------------------------------
+-- Vector.Vector
+--------------------------------------------------------------------------------
+
+-- TODO: Serialize it independently
+
 -- XXX Extremely inefficient serialization of Vector
 instance Serialize a => Serialize (Vector.Vector a) where
     {-# INLINE size #-}
@@ -178,27 +145,3 @@ instance Serialize a => Serialize (Vector.Vector a) where
 
     {-# INLINE serialize #-}
     serialize off arr val = serialize off arr (Vector.toList val)
-
-data SciL = SciL Integer Int
-$(Serialize.deriveSerialize ''SciL)
-
-{-# INLINE sciLToScientific #-}
-sciLToScientific :: SciL -> Scientific.Scientific
-sciLToScientific (SciL coeff expo) = Scientific.scientific coeff expo
-
-{-# INLINE scientificToSciL #-}
-scientificToSciL :: Scientific.Scientific -> SciL
-scientificToSciL val =
-    SciL (Scientific.coefficient val) (Scientific.base10Exponent val)
-
--- XXX Can we bypass the transformation?
-instance Serialize Scientific.Scientific where
-    {-# INLINE size #-}
-    size i val = size i (scientificToSciL val)
-
-    {-# INLINE deserialize #-}
-    deserialize off arr end =
-        fmap (sciLToScientific) <$> deserialize off arr end
-
-    {-# INLINE serialize #-}
-    serialize off arr val = serialize off arr (Scientific.base10Exponent val)
