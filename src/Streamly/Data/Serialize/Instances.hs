31be240fa2934aa1fc4aa2f3627115ff73ef801d
Adithya Kumar <adithya@composewell.com>
2023-09-20 17:47:11 +0530

    Move Serialize instance of Text into another module

--- src/Streamly/Data/Serialize/Instances.hs
+++ src/Streamly/Data/Serialize/Instances.hs
@@ -19,25 +19,17 @@ module Streamly.Data.Serialize.Instances () where
 -- Imports
 --------------------------------------------------------------------------------
 
-import Streamly.Internal.Data.Serialize (Serialize(..))
-import Data.Text.Internal (Text(..))
-import Streamly.Internal.Data.Unbox (MutableByteArray(..))
-import Data.Maybe (fromJust)
 import Data.Fixed (Fixed)
-
-import qualified Data.Text.Array as TArr (Array(..))
-import qualified Streamly.Internal.Data.Unbox as Unbox
-import qualified Streamly.Internal.Data.Serialize.TH as Serialize
-
-import Data.Time (Day, TimeOfDay, LocalTime, DiffTime, UTCTime)
-
-import GHC.Exts
-
+import Data.Maybe (fromJust)
 import Data.Scientific (Scientific)
+import Data.Time (Day, TimeOfDay, LocalTime, DiffTime, UTCTime)
+import Streamly.Data.Serialize.Instances.Text ()
+import Streamly.Internal.Data.Serialize (Serialize(..))
 
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Vector as Vector
+import qualified Streamly.Internal.Data.Serialize.TH as Serialize
 
 --------------------------------------------------------------------------------
 -- Time
@@ -57,47 +49,6 @@ $(Serialize.deriveSerialize ''UTCTime)
 --------------------------------------------------------------------------------
 
 $(Serialize.deriveSerialize ''Scientific)
-
---------------------------------------------------------------------------------
--- Text
---------------------------------------------------------------------------------
-
-instance Serialize Text where
-    size i (Text (TArr.Array _) _ len16) =
-            i + len16 * 2 + 8 -- XXX sizeof Int
-
-    {-# INLINE deserialize #-}
-    deserialize off arr end = do
-        (off1, len16) <- deserialize off arr end :: IO (Int, Int)
-        let lenBytes = len16 * 2
-
-        -- Check the available length in input buffer
-        if (off1 + lenBytes <= end)
-        then do
-            newArr <- Unbox.newBytes lenBytes
-            -- XXX We can perform an unrolled word copy directly?
-            Unbox.putSliceUnsafe arr off1 newArr 0 lenBytes
-            pure
-                ( off1 + lenBytes
-                , Text
-                      (TArr.Array
-                          (unsafeCoerce# (Unbox.getMutableByteArray# newArr)))
-                      0
-                      len16
-                )
-        else error $ "deserialize: Text: input buffer underflow: off1 = "
-                ++ show off1 ++ " lenBytes = " ++ show lenBytes
-                ++ " end = " ++ show end
-
-    {-# INLINE serialize #-}
-    serialize off arr (Text (TArr.Array barr#) off16 len16) = do
-        off1 <- serialize off arr (len16 :: Int)
-        let lenBytes = len16 * 2
-        Unbox.putSliceUnsafe
-            (MutableByteArray (unsafeCoerce# barr#)) (off16 * 2)
-            arr off1
-            lenBytes
-        pure (off1 + lenBytes)
 
 --------------------------------------------------------------------------------
 -- BSL.ByteString
